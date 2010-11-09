88e119e4b79393d7d240be50adf841fb0ee60971
Danny Mulligan <danny@dannymulligan.com>
2010-11-08 21:57:04 -0600

    Initial commit of all my solutions so far

---
+++ template.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# coding=utf-8
+#
+# Project Euler.net Problem ??
+#
+# Problem summary
+#
+# Problem text
+#
+# Answer: 
+# Solved ??/??/10
+# ?? problems solved
+# Position #??? on level ?
