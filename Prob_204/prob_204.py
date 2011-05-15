5c820b712b3c1eae3679237f4673e35109a02490
Danny Mulligan <danny@dannymulligan.com>
2011-05-14 20:28:36 -0500

    Solved problem 204

--- Prob_204/prob_204.py
+++ Prob_204/prob_204.py
@@ -17,25 +17,54 @@
 # How many generalised Hamming numbers of type 100 are there which
 # don't exceed 10^9?
 #
-# Answer: 
-# Solved ??/??/10
-# ?? problems solved
-# Position #??? on level ?
+# Answer: 2944730
+# Solved 05/14/10
+# 139 problems solved
+# Position #232 on level 3
 
-LIMIT = 10**8
-primes = [2, 3, 5]
+# There are 2 primes <=5, they are 2, 3, 5
+# There are 25 primes <=100, they are 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
 
-def hamming(pr, po):
+#LIMIT = 10**8
+LIMIT = 10**9
+
+
+def hamming(primes, powers):
     result = 1
-    for i in range(len(pr)):
-       result *= pr[i]**po[i]
+    for i in range(len(primes)):
+       result *= primes[i]**powers[i]
     return result
 
-pow = [0, 0, 0]
+def is_hamming(primes, powers):
+    if (hamming(primes,powers) <= LIMIT):
+        return True
+    else:
+       return False
 
-ans = hamming(primes, pow)
 
-while
+#primes = [2, 3, 5]
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+prime_count = len(primes)
+powers = [0] * prime_count
+
+answer = 0
+while (True):
+    res = hamming(primes,powers)
+    if (res <= LIMIT):
+        answer += 1
+        if ((answer % 10000) == 0):
+            print "{0} solutions found, hamming{1} = {2}".format(answer, powers, res)
+    
+
+    for i in range(prime_count):
+        powers[i] += 1
+        if is_hamming(primes,powers):
+            break
+        powers[i] = 0
+    if (powers == [0]*prime_count):
+        break
+
+print "Answer =", answer
 
 #primes100 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
