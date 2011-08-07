0f44b73747386279a098530e56c2859c5baff130
Danny Mulligan <danny@dannymulligan.com>
2011-08-07 13:01:05 -0500

    Solved problem 138

--- Prob_138/prob_138a.py
+++ Prob_138/prob_138a.py
@@ -45,54 +45,56 @@ import sys
 import pdb
 import cProfile
 import time
+import fractions
 
 start_time = time.clock()
 now_time = start_time
 
+def test(b,h,L):
+    if ((b % 2) != 0):  return False
+    if ((b - h)**2 != 1):  return False
+    if (((b/2)**2 + h**2) != L**2):  return False
+    return True
+
+
+def pythagorean_triple(m,n):
+    a = m**2 - n**2
+    b = 2*m*n
+    c = m**2 + n**2
+    if (a>b):
+        a,b = b,a
+    #print "m={0}, n={1}, (a,b,c) = ({2},{3},{4})".format(m,n,a,b,c)
+    return a, b, c
+
+
 ans = 0
+cnt = 0
 soln_cnt = 0
-b = 10
+m,n = 2,1
 while (soln_cnt < 12):
-    hp = b + 1
-    hm = b - 1
-    Lp2 = hp**2 + (b/2)**2
-    Lm2 = hm**2 + (b/2)**2
-    fLp = float(Lp2)**0.5
-    fLm = float(Lm2)**0.5
-    Lp = int(fLp)
-    Lm = int(fLm)
+    b,h,L = pythagorean_triple(m,n)
+    b *= 2
 
-    if ((Lp  )**2 == Lp2):
+    if test(b,h,L):
         soln_cnt += 1
-        ans += (Lp  )
-        print "Found solution #{0}: b = {1}, h = {2}, L = {3}, ans = {4}  (Lp  )".format(soln_cnt, b, hp, (Lp  ), ans)
-    if ((Lm  )**2 == Lm2):
-        soln_cnt += 1
-        ans += (Lm  )
-        print "Found solution #{0}: b = {1}, h = {2}, L = {3}, ans = {4}  (Lm  )".format(soln_cnt, b, hm, (Lm  ), ans)
+        ans += L
+        print "Found solution #{0}: b={1}, h={2}, L={3}, ans={4}, (m,n)=({5},{6})".format(soln_cnt, b, h, L, ans, m, n)
+        m,n = m+1,m
+    m += 2
 
-    if ((Lp-1)**2 == Lp2):
-        soln_cnt += 1
-        ans += (Lp-1)
-        print "Found solution #{0}: b = {1}, h = {2}, L = {3}, ans = {4}  (Lp-1)".format(soln_cnt, b, hp, (Lp-1), ans)
-    if ((Lp+1)**2 == Lp2):
-        soln_cnt += 1
-        ans += (Lp+1)
-        print "Found solution #{0}: b = {1}, h = {2}, L = {3}, ans = {4}  (Lp+1)".format(soln_cnt, b, hp, (Lp+1), ans)
-    if ((Lm-1)**2 == Lm2):
-        soln_cnt += 1
-        ans += (Lm-1)
-        print "Found solution #{0}: b = {1}, h = {2}, L = {3}, ans = {4}  (Lm-1)".format(soln_cnt, b, hm, (Lm-1), ans)
-    if ((Lm+1)**2 == Lm2):
-        soln_cnt += 1
-        ans += (Lm+1)
-        print "Found solution #{0}: b = {1}, h = {2}, L = {3}, ans = {4}  (Lm+1)".format(soln_cnt, b, hm, (Lm+1), ans)
-
-    if ((b % 20000000) == 0):
+    cnt += 1
+    if ((cnt % 1000000) == 0):
         prev_time = now_time
         now_time = time.clock()
-        print "    b = {0}, soln_cnt = {1}, time = {2}, delta = {3}".format(b, soln_cnt, now_time, now_time-prev_time)
-    b += 2
+        print "    cnt={0}, soln_cnt={1}, (m,n)=({2},{3}), time={4}, delta={5}".format(cnt, soln_cnt, m, n, now_time, now_time-prev_time)
 
 print "Answer =", ans
 print "Time taken =", time.clock() - start_time, "seconds"
+
+
+# There's a pattern here...
+# Found solution #1: b=16, h=15, L=17, ans=17, (m,n)=(4,1)
+# Found solution #2: b=272, h=273, L=305, ans=322, (m,n)=(17,4)
+# Found solution #3: b=4896, h=4895, L=5473, ans=5795, (m,n)=(72,17)
+# Found solution #4: b=87840, h=87841, L=98209, ans=104004, (m,n)=(305,72)
+# When a solution is found, the next n is the same as the previous solution m
