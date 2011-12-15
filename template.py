8ca875403ee5b8e280766ea72f448e1b2431677b
Danny Mulligan <danny@dannymulligan.com>
2011-12-15 11:50:23 -0600

    Added profiling & debugger info

--- template.py
+++ template.py
@@ -10,3 +10,19 @@
 # Solved ??/??/11
 # ?? problems solved
 # Position #??? on level ?
+
+#import cProfile
+#cProfile.run('main()')
+
+#import pdb
+#pdb.set_trace()
+
+import sys
+import time
+start_time = time.clock()
+
+########################################
+
+
+print "Time taken = {0} seconds".format(time.clock() - start_time)
+
