7e3bbeb094e1ac53146569f481daacd116f243fb
Danny Mulligan <danny@dannymulligan.com>
2014-05-17 23:37:43 -0500

    Solved #239

---
+++ Prob_239/prob_239.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# coding=utf-8
+#
+# Project Euler.net Problem 239
+#
+# Twenty-too Foolish Primes
+#
+# A set of disks numbered 1 through 100 are placed in a line in random
+# order.
+#
+# What is the probability that we have a partial derangement such that
+# exactly 22 prime number discs are found away from their natural
+# positions? (Any number of non-prime disks may also be found in or
+# out of their natural positions.)
+#
+# Give your answer rounded to 12 places behind the decimal point in
+# the form 0.abcdefghijkl.
+#
+# Solved ??/??/14
+# ??? problems solved
+# Position #??? on level ?
