ecac7586c0389ae37b74aceb81c24265a617b531
Danny Mulligan <danny@dannymulligan.com>
2016-01-23 13:50:24 -0600

    Working code for problem 381, but far too slow

--- Prob_381/prob_381.py
+++ Prob_381/prob_381.py
@@ -14,31 +14,21 @@
 #   = 872.
 # As 872 mod(7) = 4, S(7) = 4.
 #
-# It can be verified that S(p) = 480 for 5<=p<=100.
+# It can be verified that S(p) = 480 for 5 <= p <= 100.
 #
-# Find S(p) for 5<=p<=10^8.
+# Find S(p) for 5 <= p <= 10^8.
 #
-# Solved ??/??/12
-# ?? problems solved
-# Position #??? on level ?
-
-#import numpy as np
-#import scipy as sp
-#import matplotlib as mpl
-
-#import cProfile
-#cProfile.run('main()')
-
-#import pdb
-#pdb.set_trace()
 
 import sys
 import time
 start_time = time.clock()
 
 ########################################
+LIMIT = 10**5
+
+
 ########################################
-LIMIT_PRIME = 20
+LIMIT_PRIME = LIMIT
 prime_table = [1]*LIMIT_PRIME  # table of largest factor
 primes = []
 #prime_table = [1,2]*(LIMIT_PRIME/2)  # table of largest factor
@@ -60,9 +50,81 @@ def calculate_primes():
         i += 1
 
 calculate_primes()
-print "There are", len(primes), "primes less than", LIMIT_PRIME
-print "They are", primes
-print prime_table
+print("There are {} primes less than {}".format(len(primes), LIMIT_PRIME))
+if len(primes) < 30:
+    print("They are {}".format(primes))
 
-print "Time taken = {0} seconds".format(time.clock() - start_time)
 
+def is_prime(x):
+    return prime_table[x] == 1
+
+
+########################################
+def fact(n, p):
+    f = 1
+    for i in range(n, 1, -1):
+        f = (f * i) % p
+    return f
+
+#assert fact(6, 7) == (720 % 7)
+#assert fact(5, 7) == (120 % 7)
+#assert fact(4, 7) == ( 24 % 7)
+#assert fact(3, 7) == ( 6 % 7)
+#assert fact(2, 7) == ( 2 % 7)
+
+
+########################################
+
+def s(p):
+    #print("s({:3}) =".format(p)),
+    answer = 0
+
+    # fact(p-1,p) is always p-1
+    #x = fact(p-1, p)
+    #answer = (answer + x) % p
+    #print(" {:3}".format(x)),
+
+    # fact(p-2,p) is always 1
+    #x = fact(p-2, p)
+    #answer = (answer + x) % p
+    #print("+ {:3}".format(x)),
+
+    # fact(p-3,p) is always (p-1)/2
+    x = (p-1)/2
+    #x = fact(p-3, p)
+    answer = (answer + x) % p
+    #print("+ {:3}".format(x)),
+
+    # fact(p-5,p) varies
+    x = fact(p-5, p)
+    answer = (answer + x) % p
+    #print("+ {:3}".format(x)),
+
+    # fact(p-4,p) varies
+    #x = fact(p-4, p)
+    x = (x * (p - 4)) % p
+    answer = (answer + x) % p
+    #print("+ {:3}".format(x)),
+
+    #print(" = {}".format(answer))
+
+    return answer
+
+#assert s(7) == 4
+
+
+########################################
+
+answer = 0
+for p in range(5, LIMIT):
+    if (p % 1000) == 0:
+        print p
+    if not is_prime(p):
+        continue
+    x = s(p)
+    #print("s({}) = {}".format(p, x))
+    answer += x
+
+print("Answer = {}".format(answer))
+
+print("Time taken = {0} seconds".format(time.clock() - start_time))
