43a7295a21e4f6a7f42f2b5568bded749e7ce8ab
Danny Mulligan <danny@dannymulligan.com>
2016-08-09 11:10:08 -0500

    trivial

---
+++ Prob_190/prob_190.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# coding=utf-8
+#
+# Project Euler.net Problem 190
+#
+# Maximizing a weighted product
+#
+# Let Sm = (x1, x2, ... , xm) be the m-tuple of positive real numbers
+# with x1 + x2 + ... + xm = m for which Pm = x1 * x2^2 * ... * xm^m is
+# maximised.
+#
+# For example, it can be verified that [P10] = 4112 (where [ ] is the
+# integer part function).
+#
+# Find Sum[Pm] for 2 <= m <= 15.
+#
+# Solved ??/??/14
+# ??? problems solved
+# Position #??? on level ?
