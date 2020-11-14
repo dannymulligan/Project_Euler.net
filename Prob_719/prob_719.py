1f8184b2788ef3fb89dfbcd0583768a9a2b4c81a
Danny Mulligan <danny@dannymulligan.com>
2020-11-14 15:29:12 -0600

    Clean up and optimization for problem 719

--- Prob_719/prob_719.py
+++ Prob_719/prob_719.py
@@ -36,9 +36,33 @@ if False:
     for s in split(4):
         print(s)
 
+def ssplit(s):
+    #print("ssplit({})".format(s))
+    '''Return all of the possible splits of string s'''
+    yield [int(s)]
+    for x in range(1,len(s)):
+        for y in ssplit(s[x:]):
+            yield [int(s[:x])] + y
 
-Optimize = True
-if Optimize:
+def S(n):
+    '''Note: n is the square root of the n used in the problem description'''
+    snum = "{}".format(n**2)
+    for split in ssplit(snum):
+        if sum(split) == n:
+            print("sqrt({:,}) = {} = {:,}".format(n**2, split, n))
+            return True
+
+def T(nums):
+    solution = 0
+    for n in nums:
+        if S(n):
+            solution += n**2
+    return solution
+
+
+optimization = 2
+if optimization >= 1:
+    # Eliminate recursion if no solution is possible
     def ssplit(s, need):
         #print("ssplit({}, {})".format(s, need))
         '''Return all of the possible splits of string s'''
@@ -50,48 +74,65 @@ if Optimize:
                     yield [prefix] + y
 
     def S(n):
+        '''Note: n is the square root of the n used in the problem description'''
         snum = "{}".format(n**2)
         for split in ssplit(snum, n):
             if sum(split) == n:
                 print("sqrt({:,}) = {} = {:,}".format(n**2, split, n))
                 return True
-else:
-    def ssplit(s):
-        print("ssplit({})".format(s))
-        '''Return all of the possible splits of string s'''
-        yield [int(s)]
-        for x in range(1,len(s)):
-            for y in ssplit(s[x:]):
-                yield [int(s[:x])] + y
 
+elif optimization >= 2:
+    # Our number splitting technique preserves the digit sum
+    # n % 9 calculates the digit sum of n modulo 0
+    # if (n % 9) doesn't equal (n**2 % 9) then no possibility of an S-number
     def S(n):
+        '''Note: n is the square root of the n used in the problem description'''
+        if (n % 9) != (n**2 % 9):
+            return False
         snum = "{}".format(n**2)
-        for split in ssplit(snum):
+        for split in ssplit(snum, n):
             if sum(split) == n:
                 print("sqrt({:,}) = {} = {:,}".format(n**2, split, n))
                 return True
 
-debug = False
-if debug:
-    nums, solution = [2, 9, 12, 82, 91, 99], 0
+    def T(nums):
+        solution = 0
+        for n in nums:
+            if (n % 9) != (n**2 % 9):
+                continue
+            if S(n):
+                solution += n**2
+        return solution
+
+
+
+check_given_examples = False
+if check_given_examples:
+    nums = [9, 82, 91, 99]
+    for n in nums:
+        print("S({}) == {}".format(n, S(n), n**2))
+        assert S(n)
+
+    nums = range(2, 10**2 + 1)
+    solution = 41333
+    result = T(nums)
+    print("T({}) = {:,}".format(nums, result))
+    assert result == solution
 else:
-   nums, solution = range(2, 10**2 + 1),         41333  # Calculated in   0.00 seconds ->   0.00 seconds
-   nums, solution = range(2, 10**3 + 1),      10804656  # Calculated in   0.06 seconds ->   0.03 seconds
-   nums, solution = range(2, 10**4 + 1),    2818842841  # Calculated in   2.74 seconds ->   0.68 seconds
-   nums, solution = range(2, 12500 + 1),    3078722269  # Calculated in   4.56 seconds ->   0.99 seconds
-   nums, solution = range(2, 15000 + 1),    3240698798  # Calculated in   6.41 seconds ->   1.32 seconds
-   nums, solution = range(2, 10**4 * 2),    3839800575  # Calculated in  10.39 seconds ->   1.96 seconds
-   nums, solution = range(2, 10**4 * 3),    6222187932  # Calculated in  17.44 seconds ->   3.34 seconds
-   nums, solution = range(2, 10**5 + 1),  499984803177  # Calculated in 129.13 seconds ->  16.42 seconds
-   nums, solution = range(2, 10**5 * 2),  831602531877  # Calculated in ???.?? seconds ->  49.53 seconds
-   nums, solution = range(2, 10**5 * 4), 5117390432505  # Calculated in ???.?? seconds -> 131.34 seconds
-   nums, solution = range(2, 10**6 + 1), 0   # Calcualted in 447.22 seconds
+    # Run time given                                           optimization = 0,    = 1,    = 2
+    nums, solution = range(2, 10**3 + 1),      10804656  # Calculated in   0.06,   0.03,   0.00 seconds
+    nums, solution = range(2, 10**4 + 1),    2818842841  # Calculated in   2.74,   0.68,   0.00 seconds
+    nums, solution = range(2, 12500 + 1),    3078722269  # Calculated in   4.56,   0.99,   0.00 seconds
+    nums, solution = range(2, 15000 + 1),    3240698798  # Calculated in   6.41,   1.32,   0.00 seconds
+    nums, solution = range(2, 10**4 * 2),    3839800575  # Calculated in  10.39,   1.96,   0.00 seconds
+    nums, solution = range(2, 10**4 * 3),    6222187932  # Calculated in  17.44,   3.34,   3.29 seconds
+    nums, solution = range(2, 10**5 + 1),  499984803177  # Calculated in 129.13,  16.42,  17.17 seconds
+    nums, solution = range(2, 10**5 * 2),  831602531877  # Calculated in ???.??,  50.37,  47.84 seconds
+    nums, solution = range(2, 10**5 * 4), 5117390432505  # Calculated in ???.??, 131.34, 133.85 seconds
+    nums, solution = range(2, 10**6 + 1),             0  # Calcualted in ???.??, 447.22, 413.22 seconds
 
 
-Answer = 0
-for n in nums:
-    if S(n):
-        Answer += n**2
+Answer = T(nums)
 
 if solution == 0:
     print("Answer is {:,}".format(Answer))
@@ -101,15 +142,3 @@ else:
     print("ERROR: Answer is {:,} which does not match the expected solution of {:,}".format(Answer, solution))
 
 print("Time taken = {:.2f} seconds".format(time.clock() - start_time))
-
-
-# sqrt(81) = [8, 1] = 9
-# sqrt(100) = [10, 0] = 10
-# sqrt(1,296) = [1, 29, 6] = 36
-# sqrt(2,025) = [20, 25] = 45
-# sqrt(3,025) = [30, 25] = 55
-# sqrt(6,724) = [6, 72, 4] = 82
-# sqrt(8,281) = [8, 2, 81] = 91
-# sqrt(9,801) = [98, 1] = 99
-# sqrt(10,000) = [100, 0] = 100
-# Answer is 41,333 which matches the expected solution
