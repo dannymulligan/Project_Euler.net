4368aac279996e5972c479ed04fb6be23112650c
Danny Mulligan <danny@dannymulligan.com>
2020-11-22 12:05:01 -0600

    Made solution to problem 719 run faster

--- Prob_719/prob_719.py
+++ Prob_719/prob_719.py
@@ -60,7 +60,7 @@ def T(nums):
     return solution
 
 
-optimization = 2
+optimization = 3
 if optimization >= 1:
     # Eliminate recursion if no solution is possible
     def ssplit(s, need):
@@ -81,12 +81,14 @@ if optimization >= 1:
                 print("sqrt({:,}) = {} = {:,}".format(n**2, split, n))
                 return True
 
-elif optimization >= 2:
+if optimization >= 2:
     # Our number splitting technique preserves the digit sum
     # n % 9 calculates the digit sum of n modulo 0
     # if (n % 9) doesn't equal (n**2 % 9) then no possibility of an S-number
     def S(n):
         '''Note: n is the square root of the n used in the problem description'''
+        #if (n % 9) != (n**2 % 9):
+        #    return False
         if (n % 9) != (n**2 % 9):
             return False
         snum = "{}".format(n**2)
@@ -98,12 +100,35 @@ elif optimization >= 2:
     def T(nums):
         solution = 0
         for n in nums:
-            if (n % 9) != (n**2 % 9):
+            if (n % 9) > 1:
                 continue
             if S(n):
                 solution += n**2
         return solution
 
+if optimization >= 3:
+    # Our number splitting technique preserves the digit sum
+    # n % 9 calculates the digit sum of n modulo 0
+    # If (n % 9) doesn't equal 0 or 1 then no possibility of an S-number
+    # Using a appropriate range generator eliminates a lot of work
+    def S(n):
+        '''Note: n is the square root of the n used in the problem description'''
+        snum = "{}".format(n**2)
+        for split in ssplit(snum, n):
+            if sum(split) == n:
+                print("sqrt({:,}) = {} = {:,}".format(n**2, split, n))
+                return True
+
+    def T(nums):
+        solution = 0
+        for n in nums:
+            if S(n):
+                solution += n**2
+            if S(n+1):
+                solution += (n+1)**2
+        return solution
+
+    nums, solution = range(9, 10**5 + 1, 9),  499984803177  # Calculated in 129.13,  16.42,  17.17 seconds
 
 
 check_given_examples = False
@@ -119,18 +144,22 @@ if check_given_examples:
     print("T({}) = {:,}".format(nums, result))
     assert result == solution
 else:
-    # Run time given                                           optimization = 0,    = 1,    = 2
-    nums, solution = range(2, 10**3 + 1),      10804656  # Calculated in   0.06,   0.03,   0.00 seconds
-    nums, solution = range(2, 10**4 + 1),    2818842841  # Calculated in   2.74,   0.68,   0.00 seconds
-    nums, solution = range(2, 12500 + 1),    3078722269  # Calculated in   4.56,   0.99,   0.00 seconds
-    nums, solution = range(2, 15000 + 1),    3240698798  # Calculated in   6.41,   1.32,   0.00 seconds
-    nums, solution = range(2, 10**4 * 2),    3839800575  # Calculated in  10.39,   1.96,   0.00 seconds
-    nums, solution = range(2, 10**4 * 3),    6222187932  # Calculated in  17.44,   3.34,   3.29 seconds
-    nums, solution = range(2, 10**5 + 1),  499984803177  # Calculated in 129.13,  16.42,  17.17 seconds
-    nums, solution = range(2, 10**5 * 2),  831602531877  # Calculated in ???.??,  50.37,  47.84 seconds
-    nums, solution = range(2, 10**5 * 4), 5117390432505  # Calculated in ???.??, 131.34, 133.85 seconds
-    nums, solution = range(2, 10**6 + 1),             0  # Calcualted in ???.??, 447.22, 413.22 seconds
+    # Run time given                                              optimization = 0,    = 1,    = 2
+    nums, solution = range(2, 10**3 + 1),         10804656  # Calculated in   0.06,   0.03,   0.00 seconds
+    nums, solution = range(2, 10**4 + 1),       2818842841  # Calculated in   2.74,   0.68,   0.00 seconds
+    nums, solution = range(2, 12500 + 1),       3078722269  # Calculated in   4.56,   0.99,   0.00 seconds
+    nums, solution = range(2, 15000 + 1),       3240698798  # Calculated in   6.41,   1.32,   0.00 seconds
+    nums, solution = range(2, 10**4 * 2),       3839800575  # Calculated in  10.39,   1.96,   0.00 seconds
+    nums, solution = range(2, 10**4 * 3),       6222187932  # Calculated in  17.44,   3.34,   3.29 seconds
+    nums, solution = range(2, 10**5 + 1),     499984803177  # Calculated in 129.13,  16.42,  17.17 seconds
+#    nums, solution = range(2, 10**5 * 2),     831602531877  # Calculated in ???.??,  50.37,  47.84 seconds
+#    nums, solution = range(2, 10**5 * 4),    5117390432505  # Calculated in ???.??, 131.34, 133.85 seconds
+    nums, solution = range(2, 10**6 + 1),                0  # Calcualted in ???.??, 447.22, 413.22 seconds
 
+    # These versions set nums to where (n % 9) == 0 for optimization = 3
+    # They rely on the T function also calculating the (n % 9) == 1 case by adding 1 to n
+    nums, solution = range(9, 10**5 + 1, 9),  499984803177  # Calculated in  3.81 seconds
+    nums, solution = range(9, 10**6 + 1, 9),             0  # Calcualted in 89.60 seconds
 
 Answer = T(nums)
 
